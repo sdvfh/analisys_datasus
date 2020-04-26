60438f459dd6a59bf830b43d773011c6747e01f9
Sérgio Filho <sergio.vasconcelos@mv.com.br>
2020-04-25 21:48:54 -0300

    Uso de .csv externo para tratamento de nulos e exclusão de colunas.

--- codigos/analise_inicial.py
+++ codigos/analise_inicial.py
@@ -16,73 +16,20 @@ pd.set_option('display.max_rows', 100)
 df = pd.read_csv('./analisys_datasus/bases/ETLSIM.DORES_1997.csv', sep='	')
 
 # %%
-colunas_apagar = [
-    'NUMERODO',
-    'def_tipo_obito',
-    'DTOBITO',
-    'ano_obito',
-    'DTNASC',
-    'ano_nasc',
-    'idade_obito_anos',
-    'idade_obito_meses',
-    'idade_obito_dias',
-    'idade_obito_horas',
-    'idade_obito_mins',
-    'def_sexo',
-    'def_raca_cor',
-    'def_est_civil',
-    'def_escol',
-    'def_loc_ocor',
-    'def_escol_mae',
-    'def_gravidez',
-    'def_gestacao',
-    'def_parto',
-    'def_obito_parto',
-    'def_obito_grav',
-    'def_obito_puerp',
-    'def_assist_med',
-    'def_exame',
-    'def_cirurgia',
-    'def_necropsia',
-    'LINHAA',
-    'LINHAB',
-    'LINHAC',
-    'LINHAD',
-    'LINHAII',
-    'def_circ_obito',
-    'def_acid_trab',
-    'NUMERODV',
-    'def_fonte',
-    'HORAOBITO',
-    'NUMSUS',
-    'ESC2010',
-    'ESCMAE2010',
-    'NUMERODN',
-    'CRM',
-    'COMUNSVOIM',
-    'NUMEROLOTE',
-    'DTCADASTRO',
-    'STCODIFICA',
-    'CODIFICADO',
-    'VERSAOSIST',
-    'VERSAOSCB',
-    'DTRECEBIM',
-    'DTRECORIGA',
-    'ESCMAEAGR1',
-    'ESCFALAGR1',
-    'STDOEPIDEM',
-    'STDONOVA',
-    'DIFDATA',
-    'causabas_capitulo',
-    'causabas_grupo',
-    'causabas_categoria',
-    'causabas_subcategoria',
-    'res_coordenadas',
-    'ocor_coordenadas']
+dic_variaveis = pd.read_csv(
+    './analisys_datasus/documentacao/dic_variaveis.csv', sep=';').values
 
+colunas_apagar = [dic_variaveis[i, 0] for i in range(len(dic_variaveis))
+                  if dic_variaveis[i, 1] == 'x']
 df = df.drop(columns=colunas_apagar)
 
+for i in range(len(dic_variaveis)):
+    coluna, _, nulo = dic_variaveis[i]
+    if not np.isnan(nulo):
+        df[coluna] = df[coluna].replace({nulo: np.nan})
+
 # %%
+
 colunas_apagar = []
 total_linhas = df.shape[0]
 perc_nulo_max = 0.4
@@ -98,10 +45,10 @@ df = df.dropna()
 data = df[['idade_obito_calculado',
            'ocor_LATITUDE', 'ocor_LONGITUDE', 'ocor_ALTITUDE',
            'SEXO', 'RACACOR', 'ESTCIV', 'ESC']]
-del df
+
 # %%
 sm = SOMFactory().build(data.values,
-                        [100, 100],
+                        [20, 20],
                         mask=None, mapshape='planar',
                         lattice='rect',
                         normalization='var',
@@ -118,4 +65,3 @@ print("Topographic error = {0}; Quantization error = {1}".format(
 # %%
 view2D = sompy.mapview.View2D(100, 100, "rand data", text_size=14)
 view2D.show(sm, col_sz=2, which_dim="all", denormalize=True)
-
